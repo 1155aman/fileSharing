5e259825e9f2a2f37762e8ae9743edb9e46f352b
1155aman <120178825+1155aman@users.noreply.github.com>
2023-11-25 16:21:32 +0530

    first

---
+++ index.js
@@ -0,0 +1,37 @@
+const express=require('express');
+const app=express();
+const path=require('path');
+const cors=require('cors');
+
+
+
+const PORT=process.env.PORT || 4000;
+
+const connectDB=require('./config/db');
+connectDB();
+
+const corsOptions={
+    origin: process.env.ALLOWED_CLIENTS.split(",")
+}
+app.use(cors());
+app.options('*', cors());
+
+const fetchData=require('./script');
+setInterval(fetchData, 1000*60*1);
+
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+
+//routes
+
+app.use('/api/files', require('./routes/files'));
+app.use('/files', require('./routes/show'));
+app.use('/files/download', require('./routes/download'));
+app.use('/', (req, res)=>{
+    res.json({message:"hello this is banti"});
+});
+app.listen(PORT, ()=>{
+    console.log(`server is running at port no ${PORT}`);
+})
